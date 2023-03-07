8d09d9b986894af04bd680d5955fdd18e6a0b48c
Tanya Borisova <tborisova@mirosoft.com>
2023-03-07 11:33:16 +0000

    remove unused import

--- hello-world/entrypoint.py
+++ hello-world/entrypoint.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 from src.transform import example_transform
 
